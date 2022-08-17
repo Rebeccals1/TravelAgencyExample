3b291692017baa4aedd950228a05678f56c392be
Rebecca <rebecca@rebeccals.com>
2022-08-17 11:15:24 -0700

    Completed the reveal on scroll behavior.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,13 @@
 import '../styles/styles.css';
 import MobileMenu from './modules/mobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+
+new RevealOnScroll(".feature-item", 75);
+new RevealOnScroll(".testimonial", 80);
+let mobileMenu = new MobileMenu();
 
 if (module.hot) {
     module.hot.accept();
 }
 
-let mobileMenu = new MobileMenu();
+
